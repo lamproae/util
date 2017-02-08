ffe9471e8b96d7fadf8c954209d656f4b7cd3617
Jonathan Lee <lamproae@163.com>
2017-02-08 11:33:23 +0000

    Add Debug C flags

--- app/heapMain.c
+++ app/heapMain.c
@@ -3,18 +3,26 @@
 #include "leak-checker.h"
 #include "backtrace.h"
 #include "fault.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 void test2() {
+    int *j = malloc(sizeof(int));
     int *k = NULL;
     *k = 10;
+    *j /= 10;
 }
 
 void test1() {
+    int *j = malloc(sizeof(int));
     test2();
+    *j /= 10;
 }
 
 void test() {
+    int *j = malloc(sizeof(int));
     test1();
+    *j /= 10;
 }
 
 int main(int argc, char** argv)
@@ -29,4 +37,6 @@ int main(int argc, char** argv)
      i = malloc(sizeof(int));
     leak_checker_stop();
     test();
+
+    return 0;
 }
